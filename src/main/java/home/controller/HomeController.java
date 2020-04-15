7c1240bcc9356de5e711df13ca5ae8ac42613f65
frederikagger <frederikagger@hotmail.com>
2020-04-15 13:56:43 +0200

    @RequestParam added so its possible to search for a city

--- src/main/java/home/controller/HomeController.java
+++ src/main/java/home/controller/HomeController.java
@@ -4,6 +4,9 @@ import home.service.APIService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class HomeController {
@@ -14,8 +17,9 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("vejr", apiService.getVejr());
+    public String home(@RequestParam(defaultValue = "copenhagen") String city, Model model){
+        model.addAttribute("vejr", apiService.getVejr(city));
         return "index";
+
     }
 }
