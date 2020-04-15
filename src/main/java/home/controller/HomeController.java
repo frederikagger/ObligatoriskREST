b511d4ee153b33c79acdb53890666913842ea75e
frederikagger <frederikagger@hotmail.com>
2020-04-15 17:50:22 +0200

    historicaldata added to HomeController

--- src/main/java/home/controller/HomeController.java
+++ src/main/java/home/controller/HomeController.java
@@ -1,12 +1,12 @@
 package home.controller;
 
+import home.model.Vejr;
 import home.service.APIService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class HomeController {
@@ -20,6 +20,19 @@ public class HomeController {
     public String home(@RequestParam(defaultValue = "copenhagen") String city, Model model){
         model.addAttribute("vejr", apiService.getVejr(city));
         return "index";
-
     }
+
+    @GetMapping("/historicalData")
+    public String history(Model model){
+        model.addAttribute("altvejr", apiService.getAllVejrFromDB());
+        return "historicalData";
+    }
+
+    @GetMapping("/historicalData/{id}")
+    public String history(@PathVariable long id, Model model){
+        model.addAttribute("vejr", apiService.getVejrByID(id));
+        model.addAttribute("weather", apiService.getWeatherByID(id));
+        return "detailsHistoricalData";
+    }
+
 }
