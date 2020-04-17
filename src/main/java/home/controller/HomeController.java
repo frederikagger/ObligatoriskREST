6f522649f83136d884c250b00fe06f9e60db35b4
frederikagger <frederikagger@hotmail.com>
2020-04-17 16:24:06 +0200

    Some refactoring and @exceptionHandler method added

--- src/main/java/home/controller/HomeController.java
+++ src/main/java/home/controller/HomeController.java
@@ -1,9 +1,12 @@
 package home.controller;
 
+import home.exceptions.NotFoundException;
 import home.model.Vejr;
 import home.service.APIService;
+import home.service.DBService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
     private APIService apiService;
+    private DBService dbService;
 
-    public HomeController(APIService apiService) {
+    public HomeController(APIService apiService, DBService dbService) {
         this.apiService = apiService;
+        this.dbService = dbService;
     }
 
     @GetMapping("/")
@@ -24,15 +29,21 @@ public class HomeController {
 
     @GetMapping("/historicalData")
     public String history(Model model){
-        model.addAttribute("altvejr", apiService.getAllVejrFromDB());
+        model.addAttribute("altvejr", dbService.getAllVejrFromDB());
         return "historicalData";
     }
 
     @GetMapping("/historicalData/{id}")
     public String history(@PathVariable long id, Model model){
-        model.addAttribute("vejr", apiService.getVejrByID(id));
-        model.addAttribute("weather", apiService.getWeatherByID(id));
+        model.addAttribute("vejr", dbService.getVejrByID(id));
+        model.addAttribute("weather", dbService.getWeatherByID(id));
         return "detailsHistoricalData";
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public String exceptionHandling(Model model){
+        model.addAttribute("message", "404 not found");
+        return "exceptionPage";
     }
 
 }
